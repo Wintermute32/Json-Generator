d47f9873b43f7d5231b0e92eece98bf146735442
Pierce_Nudd <pierce@tictocgames.com>
2021-06-03 23:33:02 -0700

    Fully featured prototype complete. Added proper indentation and writing to Json file, writes properly indented string to premade Json which opens in Visual Studio.

--- Json Validator/JsonPopulator/JsonValidator/JsonConverters/JsonGeneration.cs
+++ Json Validator/JsonPopulator/JsonValidator/JsonConverters/JsonGeneration.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using JsonValidator.JsonConverters;
@@ -36,16 +37,19 @@ namespace JsonValidator
                 prizes = prizesConverter.GeneratePrizeList(flowBoxes),
                 tiers = tierConverter.GenerateTierList(flowBoxes),
                 lastChanceBoxPrizes = GetLastChanceList(flowBoxes),
-        };
+            };
 
-            Debug.WriteLine(SerializeJson(finalRoot));
+            Debug.WriteLine(FormatJson(finalRoot));
+            File.WriteAllText(@"C:\Users\pdnud\OneDrive\Desktop\Json Validator\[1.5.0] mystery_boxes_config - Default.json", FormatJson(finalRoot));
+            System.Diagnostics.Process.Start(@"C:\Users\pdnud\OneDrive\Desktop\Json Validator\[1.5.0] mystery_boxes_config - Default.json");
+
         }
 
         public List<LastChanceBoxPrize> GetLastChanceList(List<FlowLayoutPanel> flowlist)
         {
             var popLists = flowlist.Find(x => x.Name == "lastChanceBoxPanel").Controls.OfType<GroupBox>().ToList();
             List<LastChanceBoxPrize> lastChanceList = new List<LastChanceBoxPrize>();
-            
+
             foreach (var x in popLists)
             {
                 var combos = x.Controls.OfType<ComboBox>().ToList();
@@ -61,7 +65,7 @@ namespace JsonValidator
         }
         public List<string> GetFeaturedPops(List<FlowLayoutPanel> flowList)
         {
-             var popLists = flowList.Find(x => x.Name == "storePopsPanel").Controls.OfType<ComboBox>().ToList();
+            var popLists = flowList.Find(x => x.Name == "storePopsPanel").Controls.OfType<ComboBox>().ToList();
             List<string> _popIds = new List<string>();
 
             foreach (var x in popLists) //break me into a new class with store appearance
@@ -71,8 +75,8 @@ namespace JsonValidator
             return _popIds;
         }
 
-      public string SerializeJson(NewRoot newRoot)
-      {
+        public string SerializeJson(NewRoot newRoot)
+        {
             //Accepts Completed NewRoot Object
             var serializerSettings = new JsonSerializerSettings();
 
@@ -82,6 +86,39 @@ namespace JsonValidator
             });
 
             return rootOutput;
-      }
+        }
+
+        public string FormatJson(NewRoot newRoot)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+            };
+
+            settings.Converters.Add(new MyConverter());
+
+            string json = JsonConvert.SerializeObject(newRoot, settings);
+            return json;
+        }
+
     }
+    class MyConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) //controlling what types are affected with formatting, indented or none
+        {
+            return objectType == typeof(string[]) || objectType == typeof(Tier) || objectType == typeof(Prize) 
+                || objectType == typeof(List<string>) || objectType == typeof(LastChanceBoxPrize);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(JsonConvert.SerializeObject(value, Formatting.None));
+        }
+    }
+
 } 

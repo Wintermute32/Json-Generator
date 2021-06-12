ee86f76a2f84dc7cb26abe8671336f5384891489
Pierce_Nudd <pierce@tictocgames.com>
2021-06-12 14:02:17 -0700

    Commit to update Refactoring_Json

---
+++ Json Validator/JsonPopulator/JsonValidator/JsonClasses/NewRoot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JsonValidator.CSV;
+using JsonValidator.Json_Classes;
+
+namespace JsonValidator
+{
+    public class NewRoot
+    {
+        public string boxId { get; set; }
+        public string fandomId { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public Appearance appearance { get; set; }
+        public string behaviourType { get; set; }
+        public List<string> featuredPopIdsList { get; set; }
+        public List<Prize> prizes { get; set; }
+        public List<Tier> tiers { get; set; }
+        public List<LastChanceBoxPrize> lastChanceBoxPrizes { get; set; }
+
+        List<string> rarityList = new List<string>() { "common", "rare", "epic", "legendary" };
+        JsonClassGenHelpers JGenHelper = new JsonClassGenHelpers();
+
+        public NewRoot() { }
+
+        public NewRoot(Playbook playbook, Dictionary<string, string> popDict)
+        {
+            behaviourType = "";
+            boxId = JGenHelper.AmmendBoxId(playbook, true);
+            fandomId = playbook.fandomName;
+            FixDates(playbook.startDate, playbook.endDate);
+            SetFeaturedPopIds(popDict);
+        }
+  
+        public void FixDates(string startDate, string endDate)
+        {
+
+           var revisedStart = DateTime.Parse(startDate).ToString("MM/dd/yyyy HH:mm");
+           var revisedEnd = DateTime.Parse(endDate).AddDays(-1).ToString("MM/dd/yyyy HH:mm");
+
+            this.startDate = revisedStart;
+            this.endDate = revisedEnd;
+        }
+
+        public void SetFeaturedPopIds(Dictionary<string, string> popIdDict)
+        {
+            foreach (KeyValuePair<string, string> entry in popIdDict)
+                for (int i = 0; i < rarityList.Count; i++)
+                    if (entry.Value == rarityList[i])
+                        rarityList[i] = entry.Key;
+
+            featuredPopIdsList = rarityList;
+        }
+    }
+}
